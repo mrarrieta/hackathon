58c9bc961accda4202b979f86ba203cec80e9d86
alfredo.arrieta <alfredo.arrieta@wizeline.com>
2025-03-31 16:17:24 -0500

    added record dao

---
+++ base/home/lib/data/record_dao.dart
@@ -0,0 +1,25 @@
+import 'dart:core';
+
+import 'package:floor/floor.dart';
+import 'package:home/data/record.dart';
+
+@dao
+abstract class RecordDao{
+
+  //on changes run: flutter packages pub run build_runner build --delete-conflicting-outputs
+
+  @Query('SELECT * FROM Record WHERE referenceId = 1 ORDER BY time DESC')
+  Future<List<Record>> findAllRecords();
+
+  @Query('SELECT * FROM Record WHERE referenceId = :parentId ORDER BY time DESC')
+  Future<List<Record>> findAllRecordsByParent(String parentId);
+
+  @insert
+  Future<void> insertRecord(Record record);
+
+  @update
+  Future<void> updateRecord(Record record);
+
+  @delete
+  Future<void> deleteRecord(Record record);
+}
