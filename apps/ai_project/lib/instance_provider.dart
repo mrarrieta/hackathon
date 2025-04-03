8490a21d5dd0377436382080d9829cdec8ab4b48
alfredo.arrieta <alfredo.arrieta@wizeline.com>
2025-04-03 14:08:11 -0500

    added bloc as state management for login

--- apps/ai_project/lib/instance_provider.dart
+++ apps/ai_project/lib/instance_provider.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:home/data/record_repository.dart';
 import 'package:home/tools/date_tools.dart';
 import 'package:login/data/login_api.dart';
-import 'package:login/data/login_view_model.dart';
 import 'package:signup/data/signup_api.dart';
 import 'package:signup/data/signup_view_model.dart';
 import 'package:splash/data/splash_api.dart';
@@ -10,10 +9,9 @@ import 'package:splash/data/splash_api.dart';
 class InstanceProvider {
   static final DateTools _dateTools = DateTools();
   static final Dio _dio = Dio(BaseOptions(contentType: "application/json"));
-  static final LoginClient _loginClient = LoginClient(_dio);
+  static final LoginClient loginClient = LoginClient(_dio);
   static final SignupClient _signupClient = SignupClient(_dio);
   static final SplashClient splashClient = SplashClient(_dio);
-  static final LoginViewModel loginViewmodel = LoginViewModel(_loginClient);
   static final SignupViewModel signupViewModel = SignupViewModel(_signupClient);
   static final RecordRepository recordRepository = RecordRepository(_dateTools);
 }
